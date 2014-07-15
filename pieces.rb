399a06a96742ed85ceb9eb6cacae60b84820f176
Noah and Adam <Noah+Adam@appacademy.io>
2014-07-15 10:45:57 -0400

    Define SlidingPieces.

---
+++ pieces.rb
@@ -0,0 +1,23 @@
+class Piece
+
+end
+
+class SlidingPiece < Piece
+
+end
+
+class SteppingPiece < Piece
+
+end
+
+class Queen < SlidingPiece
+
+end
+
+class Bishop < SlidingPiece
+
+end
+
+class Rook < SlidingPiece
+
+end
